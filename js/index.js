3d9ce68576bcf93615fe94395a20892c0e1f95bd
const <103208522+cons8@users.noreply.github.com>
2024-11-16 23:36:20 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-let test_btn =  document.querySelector("#test_btn");
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
-test_btn.addEventListener("click", function(){
-    alert("试试就试试");
-});
+createApp({
+    data(){
+        return {
+           works : [{ 
+                name : "时序",
+                desc : "高效时间管理",
+                bg : "red"
+            },
+            {   name : "补光灯",
+                desc : "照亮你的美"
+            }],
+            navs: [
+                {name : "首页", url : "/","sronly":true},
+                {name : "补光灯", url : "./htmls/light.html","sronly":false},
+                {name : "链接", url : "/htmls/links.html","sronly":false},
+             ]
+        }
+    },
+    methods : {
+        test(){
+            alert("试试就试试");
+        }
+    }
+}).mount("#app")

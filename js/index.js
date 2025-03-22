93fab7917c7dae760be3513294f97b33bd67fa85
const <ssk5k5@qq.com>
2025-03-23 01:31:01 +0800

    更新了网站内容

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import { getnav } from '/js/nav.js';
 
 const navs = getnav("首页")
 
+const book01 = document.querySelector("#books").children[0]
 
 createApp({
     data(){
@@ -13,7 +14,6 @@ createApp({
     methods : {
         test(){
             alert("试试就试试");
-    
         }
     }
 }).mount("#app")
